64a360a3e3cf2d7cb956f40e95152fb19cfb98b4
syakillasaals <syakillasalsa7@gmail.com>
2024-12-20 00:56:49 +0700

    Update page.tsx

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,6 @@
 export default function page() {
-<<<<<<< HEAD
     return 
         <p>This is the customers page.</p>;
-=======
-    return <p>This is the customers page.</p>;
->>>>>>> fa7c0a6189141873d94c0f4d50a6bb21444876b4
      
   }
   
