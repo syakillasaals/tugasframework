de2e3e7b26f91ef57b98a115d8003e042358db3c
syakillasaals <syakillasalsa7@gmail.com>
2024-12-20 00:49:34 +0700

    Save local changes before pulling

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,6 @@
-export default function CustomersPage() {
-    return (
-      <div>
-        <h1>Customers Dashboard</h1>
-        <p>This is the customers page.</p>
-      </div>
-    );
+export default function page() {
+    return 
+        <p>This is the customers page.</p>;
+     
   }
   
